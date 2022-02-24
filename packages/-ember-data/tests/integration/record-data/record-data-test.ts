c3f6bdeb81fff38e5faf4cc4ad685b36c7f0314a
Scott Newcomer <snewcomer24@gmail.com>
2022-02-24 08:14:17 -0600

    bug: new records are improperly reloaded when relationships are set

--- packages/-ember-data/tests/integration/record-data/record-data-test.ts
+++ packages/-ember-data/tests/integration/record-data/record-data-test.ts
@@ -328,7 +328,7 @@ module('integration/record-data - Custom RecordData Implementations', function (
     assert.strictEqual(calledPush, 0, 'Did not call pushData');
   });
 
-  test('Record Data attribute settting', async function (assert) {
+  test('Record Data attribute setting', async function (assert) {
     let expectedCount = 15;
     assert.expect(expectedCount);
     const personHash = {
